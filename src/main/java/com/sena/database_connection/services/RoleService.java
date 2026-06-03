f74fae3e9b855c5c41dd7058f63181d9e1ac0cc3
durandavid159-art <durandavid159@gmail.com>
2026-06-03 17:23:10 -0500

    DP estructructura de la clase userServis

--- src/main/java/com/sena/database_connection/services/RoleService.java
+++ src/main/java/com/sena/database_connection/services/RoleService.java
@@ -1,12 +1,25 @@
 package com.sena.database_connection.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.sena.database_connection.entities.Role;
 import com.sena.database_connection.repositories.RoleRepository;
 
 
 public class RoleService {
+
     private RoleRepository repository;
 
     public RoleService (RoleRepository repository){
         this.repository = repository;
+    }
+
+    public List <Role> obtenerTodos (){
+        return this.repository.findAll();
+    }
+
+    public Optional <Role> porId (Long id){
+        return this.repository.findById(id);
     }
 }
