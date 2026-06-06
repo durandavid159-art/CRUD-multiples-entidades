1d4b1b539e008b60a423e1e52ca7775d4621c361
durandavid159-arte <durandavid159@gmail.com>
2026-06-05 23:07:12 -0500

    DP creo el controlador con cada uno de sus metodos y tambien creo el constructor de la clase RoleDto

--- src/main/java/com/sena/database_connection/controllers/RoleController.java
+++ src/main/java/com/sena/database_connection/controllers/RoleController.java
@@ -1,5 +1,37 @@
 package com.sena.database_connection.controllers;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import com.sena.database_connection.dtos.RoleDto;
+import com.sena.database_connection.entities.Role;
+import com.sena.database_connection.services.RoleService;
+
 public class RoleController {
-    
+    private RoleService service;
+
+    public RoleController (RoleService service) {
+        this.service = service;
+    }
+
+    public List<Role> get(){
+        return this.service.obtenerTodos();
+    }
+
+    public ResponseEntity<Role> create(RoleDto body){
+        Role role = new Role();
+        role.setName(body.getName());
+
+        Role roleCreado = this.service.crear(role);
+        return ResponseEntity.status(201).body(roleCreado);
+    }
+
+    public ResponseEntity<Role> delete (Long id){
+        Role roleEliminado = this.service.eliminar(id);
+        if (roleEliminado == null){
+            return ResponseEntity.status(404).body(null);
+        }
+        return ResponseEntity.status(200).body(roleEliminado);
+    }
 }
--- src/main/java/com/sena/database_connection/dtos/RoleDto.java
+++ src/main/java/com/sena/database_connection/dtos/RoleDto.java
@@ -1,5 +1,8 @@
 package com.sena.database_connection.dtos;
 
+import lombok.Data;
+
+@Data
 public class RoleDto {
     private String name;
 }
